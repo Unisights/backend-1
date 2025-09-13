d110d7524bac6c95ae92df9639c3def83dcb97f7
rakeshallampally657 <rakeshallampally657@gmail.com>
2025-09-13 19:24:49 +0200

    ui works

--- src/main/java/com/unisights/backend/controller/PageController.java
+++ src/main/java/com/unisights/backend/controller/PageController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
-    // @GetMapping("/auth/login")  public String login(){ return "auth/login"; }
-    // @GetMapping("/auth/signup") public String signup(){ return "auth/signup"; }
+     @GetMapping("/auth/login")  public String login(){ return "auth/login"; }
+     @GetMapping("/auth/signup") public String signup(){ return "auth/signup"; }
     @GetMapping("/programs")    public String programs(){ return "programs"; }
 }

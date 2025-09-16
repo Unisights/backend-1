d91fd1c434074119c49273199de62d236e4ddecf
rakesh.allampally <rakeshallampally657@gmail.com>
2025-09-16 18:52:29 +0200

    finished frontend

--- src/main/java/com/unisights/backend/controller/HomeController.java
+++ src/main/java/com/unisights/backend/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping("/ra")
+    @GetMapping("/")
     public String home(Model model) {
         model.addAttribute("title", "Unisights");
         return "home";  // looks for templates/home.html
